c93f7748c61597cde1f21dffa58d0753aeeec670
DangelGlobant <daniel30081990@gmail.com>
2022-05-26 11:25:17 -0400

    20. Destructuring Arrays

--- 01-ts-intro/src/index.ts
+++ 01-ts-intro/src/index.ts
@@ -30,7 +30,14 @@ const autor = 'Daniel';
 const { volumen, segundo, cancion, detalles:{ autor: autorDetalle } } = reproductor;
 // const { autor } = detalles;
 
-console.log('El volumen actual de: ', volumen);
-console.log('El segundo actual de: ', segundo);
-console.log('La canción actual de: ', cancion);
-console.log('El autor es: ', autorDetalle);
+// console.log('El volumen actual de: ', volumen);
+// console.log('El segundo actual de: ', segundo);
+// console.log('La canción actual de: ', cancion);
+// console.log('El autor es: ', autorDetalle);
+
+const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+const [ , , p3 ] = dbz;
+
+// console.log('Personaje 1:', p1 );
+// console.log('Personaje 2:', p2 );
+console.log('Personaje 3:', p3 );
