77416b95db24d9bcce94961a9c5b816d67f0b810
DangelGlobant <daniel30081990@gmail.com>
2022-05-26 13:20:50 -0400

    25. Extend a class

--- 01-ts-intro/src/index.ts
+++ 01-ts-intro/src/index.ts
@@ -3,18 +3,3 @@
     ===== Código de TypeScript =====
 */
 
-class Heroe {
-    //  public alterEgo: string;
-    //  public edad: number;
-    //  public nombreReal: number;
-
-     constructor(
-         public alterEgo: string,
-         public edad?: number,
-         public nombreReal?: string
-         ) {}
-}
-
-const ironman = new Heroe('Ironman', 45, 'Tony');
-
-console.log(ironman);
