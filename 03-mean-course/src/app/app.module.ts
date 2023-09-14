1461e6c634626852e6ea13a2351178bcf5546cc4
Daniel <daniel30081990@gmail.com>
2023-09-14 00:08:51 -0400

    21. Adding a Toolbar

--- 03-mean-course/src/app/app.module.ts
+++ 03-mean-course/src/app/app.module.ts
@@ -6,14 +6,17 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +25,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     MatInputModule,
     MatFormFieldModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
